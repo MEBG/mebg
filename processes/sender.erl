ee55ac794b863a079c83a1c5e8ae442043b462bf
Bartek Borowinski <bborowin@gmail.com>
2014-10-20 12:29:54 -0400

    changed to send real sms

--- processes/sender.erl
+++ processes/sender.erl
@@ -3,16 +3,17 @@
 -module(sender).
 -export([send/2,sms/2]).
 
-send(Number, Message) ->
-   io:format("SMS sent to ~p: ~p~n", [Number,Message]).
-
 sms(Number, Message) ->
+   io:format("SMS to be sent to ~p: ~p~n", [Number,Message]).
+
+send(Number, Message) ->
    {Username, Password} = secrets:twilio_auth(),
    UrlBase = "api.twilio.com/2010-04-01/Accounts/ACa66dd367e3772ec8696f298dd7e528a5/Messages.json",
    Url = ["https://", Username, ":", Password, "@", UrlBase],
-   Parameters = ["From=%2B",secrets:twilio_number(),"&To=",Number, "&Body=", Message],
+   Parameters = ["From=",secrets:twilio_number(),"&To=",Number, "&Body=", Message],
    httpc:request(post, 
        {lists:flatten(Url), [], 
        "application/x-www-form-urlencoded",
        lists:flatten(Parameters)
-       }, [], []).
+       }, [], []),
+   io:format("SMS to ~p sent: ~p~n", [Number,Message]).
