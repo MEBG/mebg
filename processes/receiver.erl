09019b7d12c85ed50fc930d12088cb706219b6e4
Bartek Borowinski <bborowin@gmail.com>
2014-10-21 19:58:17 -0400

    filtering out plus symbol from incoming phone number / only accept those numbers

--- processes/receiver.erl
+++ processes/receiver.erl
@@ -5,12 +5,13 @@
 
 loop() ->
    receive
-      {Number, Body} ->
+      {[43|Number], Body} -> % filters out the + from phone number
          % lowercase + tokenize body of message
          Tokens = string:tokens(string:to_lower(Body), " "),
          [A|Arguments] = Tokens,
          Action = list_to_atom(A),
          Role = db:get_role(Number),
+         io:format("~p (~p) sent ~p~n", [Number,Role,Tokens]),
          % forward parsed message to shop process
          coop ! {Number, Role, Action, Arguments},
          loop()
