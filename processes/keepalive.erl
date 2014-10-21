b582a9c636f2a5e713f571afa6da6e7f920420cb
Bartek Borowinski <bborowin@gmail.com>
2014-10-21 00:34:50 -0400

    added admin notify on keepalive restart

--- processes/keepalive.erl
+++ processes/keepalive.erl
@@ -1,6 +1,6 @@
 % for launching registered processes that must keep running
 -module(keepalive).
--export([launch/4]).
+-export([launch/4, launch/0]).
 
 launch(Name, Module, Fun, Args) ->
    Pid = spawn(Module,Fun,Args),
@@ -9,8 +9,16 @@ launch(Name, Module, Fun, Args) ->
       Ref = monitor(process, Pid),
       receive
          {'DOWN', Ref, process, Pid, Why} ->
+            Msg = lists:concat(["Process '",Name, "' crashed."]),
+            sender:send(secrets:admin_number(), Msg),
             io:format("~p (~p) died: ~p~n", [Pid,Name,Why]),
             io:format("restarting ~p~n", [Name]),
             launch(Name,Module,Fun,Args)
       end
    end).
+
+% launch all registered processes
+launch() ->
+   launch(box,cashbox,loop,[0]),
+   launch(coop,shop,loop,[#{}]),
+   launch(rcvr,receiver,loop,[]).
--- processes/secrets.erl
+++ processes/secrets.erl
@@ -2,4 +2,5 @@
 -export([twilio_auth/0, twilio_number/0]).
 
 twilio_auth() -> {"username","password"}.
-twilio_number() -> "+18885551234".
+twilio_number() -> "18885551234".
+admin_number() -> "18008008080".
