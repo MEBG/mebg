0bd99eb4af4939c0dc0b021be09fc2e90566d66c
Bartek Borowinski <bborowin@gmail.com>
2014-10-28 23:19:19 -0400

    added regression tests for volunteer sign in and sign out

--- processes/greetings.erl
+++ processes/greetings.erl
@@ -1,39 +1,43 @@
 -module(greetings).
--export([get_random/1, hello/0, bye/0, open/1, closed/0]).
+-export([
+   hello_phrases/0, hello/0,
+   bye_phrases/0, bye/0,
+   open_phrases/1, open/1,
+   closed_phrases/0, closed/0]).
 
 get_random(List) ->
    [R|_] = [X||{_,X} <- lists:sort([ {random:uniform(), N} || N <- List])],
    R.
 
+hello_phrases() -> [
+   "Welcome!",
+   "Thought you'd never drop by!",
+   "It's been a while, pardner",
+   "Nice to see you",
+   "Where have YOU been?",
+   "Once more unto the breach",
+   "Nice day!",
+   "G'day mate",
+   "A good day to wrench around..",
+   "Uhh, sorry about the mess..",
+   "Hi. I think we're out of chain breakers..",
+   "Bienvenidos compadre!"
+].
 hello() ->
-   M = [
-      "Welcome!",
-      "Thought you'd never drop by!",
-      "It's been a while, pardner",
-      "Nice to see you",
-      "Where have YOU been?",
-      "Once more unto the breach",
-      "Nice day!",
-      "G'day mate",
-      "A good day to wrench around..",
-      "Uhh, sorry about the mess..",
-      "Hi. I think we're out of chain breakers..",
-      "Bienvenidos compadre!"
-   ],
-   get_random(M).
+   get_random(hello_phrases()).
 
+bye_phrases() -> [
+   "So long!",
+   "Thanks for bringin' it brah!",
+   "And good riddance to you, sir!",
+   "Til next time!",
+   "Good times.. see ya!",
+   "Sheesh, thought you'd never leave..",
+   "Until we meet again..",
+   "Take some of the empties with ya, eh?"
+].
 bye() ->
-   M = [
-      "So long!",
-      "Thanks for bringin' it brah!",
-      "And good riddance to you, sir!",
-      "Til next time!",
-      "Good times.. see ya!",
-      "Sheesh, thought you'd never leave..",
-      "Until we meet again..",
-      "Take some of the empties with ya, eh?"
-   ],
-   get_random(M).
+   get_random(bye_phrases()).
 
 isare([_H]) ->
    " is ";
@@ -47,22 +51,22 @@ concatenate_names(String, [T]) ->
    lists:concat([String, " and ", T]);
 concatenate_names(String, [H|T]) ->
    concatenate_names(lists:concat([String, ", ", H]), T).
+open_phrases(Names) -> [
+   ["The bike shop is open, with ", concatenate_names(Names), " on duty"],
+   ["Yes, we're open! ", concatenate_names(Names), isare(Names), "here."],
+   ["Open for business - ", concatenate_names(Names), isare(Names), "helping out."],
+   [concatenate_names(Names), isare(Names), "running the show. Come on by!"],
+   [concatenate_names(Names), isare(Names), "at the shop. Come on by!"]
+].
 open(Names) ->
-   M = [
-      ["The bike shop is open, with ", concatenate_names(Names), " on duty"],
-      ["Yes, we're open! ", concatenate_names(Names), isare(Names), "here."],
-      ["Open for business - ", concatenate_names(Names), isare(Names), "helping out."],
-      [concatenate_names(Names), isare(Names), "running the show. Come on by!"],
-      [concatenate_names(Names), isare(Names), "at the shop. Come on by!"]
-   ],
-   lists:flatten(get_random(M)).
+   lists:flatten(get_random(open_phrases(Names))).
 
+closed_phrases() -> [
+   "The bike shop is closed right now",
+   "Sorry, we are not open right now",
+   "Usually open from 6pm to 9pm on weekdays.. But, not right now, sorry!",
+   "There is noone here - the bike shop is closed",
+   "Sorry, we're not here right now.."
+].
 closed() ->
-   M = [
-      "The bike shop is closed right now",
-      "Sorry, we are not open right now",
-      "Usually open from 6pm to 9pm on weekdays.. But, not right now, sorry!",
-      "There is noone here - the bike shop is closed",
-      "Sorry, we're not here right now.."
-   ],
-   get_random(M).
+   get_random(closed_phrases()).
--- processes/test.erl
+++ processes/test.erl
@@ -23,7 +23,45 @@ trap(Timeout, TriggerFun) ->
       error
    end.
 
-status_test() ->
-   Number = "15145551234",
-   {Number,Message} = trap(350,fun() -> rcvr!{lists:flatten(["+"|Number]),"status"} end),
-   Message.
+
+send(Number,Message) ->
+   trap(100, fun() -> 
+      rcvr!{lists:flatten(["+"|Number]), Message} end
+   ).
+
+t100_status_test() ->
+   Number = "111",
+   {Number,Message} = send(Number,"status"),
+   [Message] = [M||M<-greetings:closed_phrases(), M == Message].
+
+t200_arrive_test() ->
+   Number = "111",
+   {Number,M1} = send(Number,"arrive"),
+   [M1] = [M||M<-greetings:hello_phrases(), M == M1],
+   {Number,M2} = send(Number,"status"),
+   [M2] = [lists:flatten(M)||M<-greetings:open_phrases(["TV01"]), lists:flatten(M) == M2].
+   
+t300_depart_test() ->
+   Number = "111",
+   {Number,M1} = send(Number,"depart"),
+   [M1] = [M||M<-greetings:bye_phrases(), M == M1],
+   {Number,M2} = send(Number,"status"),
+   [M2] = [M||M<-greetings:closed_phrases(), M == M2].
+
+t310_depart_test() ->
+   Number = "111",
+   error = send(Number,"depart").
+
+t400_double_arrive_test() ->
+   {"111",_} = send("111","arrive"),
+   {"222",_} = send("222","arrive"),
+   {"111",Message} = send("111","status"),
+   [Message] = [lists:flatten(M)||M<-greetings:open_phrases(["TV01", "TV02"]), lists:flatten(M) == Message].
+
+t410_double_depart_test() ->
+   {"111",_} = send("111","depart"),
+   {"111",M1} = send("111","status"),
+   [M1] = [lists:flatten(M)||M<-greetings:open_phrases(["TV02"]), lists:flatten(M) == M1],
+   {"222",_} = send("222","depart"),
+   {"111",M2} = send("111","status"),
+   [M2] = [M||M<-greetings:closed_phrases(), M == M2].
