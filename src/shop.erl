824dce83c438234559639f9ff65ca95b9575c408
Bartek Borowinski <bborowin@gmail.com>
2014-11-05 23:34:01 -0500

    added hours to schedule string

--- src/shop.erl
+++ src/shop.erl
@@ -155,7 +155,7 @@ loop(Present) ->
             Message == [] ->
                sms!{send,Number,"The schedule is empty, no one has signed up."};
             true ->
-               sms!{send,Number,Message}
+               sms!{send,Number,lists:flatten(["18h-21h", 10, Message])}
          end,
          loop(Present);
 
