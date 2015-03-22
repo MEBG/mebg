55605c8f3dfc38b1b3e56f4e9d60a34327dd0b95
Bartek Borowinski <bborowin@gmail.com>
2015-03-22 12:04:31 -0400

    commented on message strings

--- src/message_strings_en.erl
+++ src/message_strings_en.erl
@@ -2,6 +2,7 @@
 % strings comprising various messages sent by system
 %
 
+
 {default, {concatenate, [
    "mile end bike garage",10,
    "135 rue Van Horne #201",10,
@@ -9,6 +10,7 @@
    "http://bikegarage.org"
 ]}}.
 
+% sent in response to a volunteer signing in
 {hello, {random, [
    "Welcome!",
    "Thought you'd never drop by!",
@@ -23,6 +25,7 @@
    "Bienvenidos compadre!"
 ]}}.
 
+% sent in response to a volunteer signing out
 {bye, {random, [
    "So long!",
    "Thanks for bringin' it brah!",
@@ -34,12 +37,14 @@
    "Take some of the empties with ya, eh?"
 ]}}.
 
+% sent to a volunteer after automated sign out
 {timeout, {random, [
    "Schedule bot signed you out",
    "It's a little latte, imma call it a night",
    "Probly a bit past closing time, gonna sign you out now"
 ]}}.
 
+% sent in response to a "schedule" query when nobody is volunteering
 {empty, {random, [
    "The schedule is empty, no one has signed up. Shop's closed.",
    "Looks like no one is signed up for a shift. That's sad..",
@@ -47,6 +52,7 @@
    "Bike shop is closed until someone signs up for a shift"
 ]}}.
 
+% response to "schedule" when there are shifts scheduled
 {schedule,
    {random,
       {build, {[null, null], ""}, [
@@ -55,6 +61,7 @@
    }
 }.
 
+% response to volunteer after adding/removing days to schedule
 {days,
    {random,
       {build, {[null, null], " and "}, [
@@ -63,23 +70,27 @@
       [{"It says here you're doing "}, {subject}]
 ]}}}.
 
+% response when volunteer removes a day and is not signed up for any
 {notsignedup, {random, [
    "You're not signed up for any shifts.",
    "You are not signed up for any days of the week"
 ]}}.
 
+% response to "status" when someone is scheduled but nobody signed in
 {late, {random, [
    "Usually open from 6pm to 9pm.. But, not right now, sorry!",
    "The bike coop is supposed to be open right now, but no one is here yet. Our apologies..",
    "Sorry, we should be open but no volunteers showed up yet.."
 ]}}.
 
+% response to "status" when no one is signed up for current day
 {shut, {random, [
    "The bike shop is not open today.",
    "Sorry, today we are closed - txt 'hours' for business hours.",
    "Closed today - txt 'hours' to see when we're open."
 ]}}.
 
+% response to "status" when volunteers are signed in
 {open,
    {random,
       {build, {[" is ", " are "], " and "}, [
@@ -92,18 +103,21 @@
    }
 }.
 
+% sent to a signed-in volunteer when someone is locked out downstairs
 {door_open,
    {random, [
       "Downstairs door is locked again..",
       "Someone's locked out downstairs"
 ]}}.
 
+% response sent to locked out person when noone is signed in
 {door_closed,
    {random, [
       "Sorry, no one is here right now.",
       "The bike shop is closed."
 ]}}.
 
+% response sent to locked out person when a volunteer is present
 {door_wait,
    {random, [
       "Not again! Nofified a volunteer, someone should be down in a minute.",
